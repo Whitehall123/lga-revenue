9e183b0474c72317debb4e9329e4d969b70a9917
Whitehall123 <enterprisewhitehall@gmail.com>
2026-08-02 10:11:11 +0100

    Create Home component for Akwa Ibom Revenue Portal

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+tsx
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "system-ui" }}>
+      <h1>Akwa Ibom Revenue Portal</h1>
+      <p>Portal is running. You can now add your components back.</p>
+    </main>
+  );
+}
